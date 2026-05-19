d29e59d739980bb7304854584ef81bafd6465a65
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-05-19 09:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (10.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // if(s.size()!=t.size()){
+        //     return false;
+        // }
+        // unordered_map<char,int> mp;
+        // for(char ch:s) {
+        //     mp[ch]++;
+        // }
+        // for(char ch: t){
+        //     mp[ch]--;
+        // }
+        // for(auto it : mp){
+        //     if(it.second!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int> mp;
-        for(char ch:s) {
-            mp[ch]++;
+        unordered_map<char,int > mp1;
+        unordered_map<char,int > mp2;
+        for(auto ch : s){
+            mp1[ch]++;
         }
-        for(char ch: t){
-            mp[ch]--;
+        for(auto it : t){
+            mp2[it]++;
         }
-        for(auto it : mp){
-            if(it.second!=0){
-                return false;
-            }
+        if(mp1==mp2){
+            return true;
         }
-        return true;
+
+        return false;
     }
 };
