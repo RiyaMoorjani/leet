2f00e02571bbe53d0902a8a920290281b7158e92
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-05-22 05:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (66.01%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    // variable sliding window 
     int lengthOfLongestSubstring(string s) {
-        int l = 0, r = 0, maxlen = 0;
+        int i = 0, j = 0, maxlen = 0;
         int n = s.length();
         vector<int> hash(256, -1);
 
-        while(r < n) {
+        while(j < n) {
+            //alaredy occured character
+            if(hash[s[j]]!=-1){//not equal to - 1means it has occured laready 
+                if(hash[s[j]]>=i){//means it is also a part of window 
+                    i=hash[s[j]]+1;
+                }
 
-            if(hash[s[r]] >= l) {
-                l = hash[s[r]] + 1;
             }
-            int len = r - l + 1;
-            maxlen = max(maxlen, len);
-
-            hash[s[r]] = r;
-            r++;
+            int len =j-i+1;
+            maxlen=max(maxlen,len);
+            //srore the index of the value
+            hash[s[j]]=j;
+            j++;
         }
 
         return maxlen;
