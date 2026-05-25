2d8887bb9a2480873e7853ce9c7429e3395c4331
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-05-25 09:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (60.21%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     void solve(vector<int>& candidates, int target,vector<vector<int>>& ans,vector<int> &op,int i,int n){
+        //BC
         if(target==0){
             ans.push_back(op);
             return;
 
         }
-        for(int idx=i;idx<n;idx++){
-
-            if(idx>i && candidates[idx]==candidates[idx-1]){
-                continue;
-            }
-            if(candidates[idx]>target){
-                break;
-            }
-            op.push_back(candidates[idx]);
-            solve(candidates,target-candidates[idx],ans,op,idx+1,n);
-            op.pop_back();
-
+        if(i>=n || target<0){
+            return ;
         }
+        
+        if(candidates[i]>target){
+            return;
+        }
+        //include
+        op.push_back(candidates[i]);
+        solve(candidates,target-candidates[i],ans,op,i+1,n);
+        op.pop_back();//backtrack
+        //check duplicate
+        while(i+1<n && candidates[i]==candidates[i+1]){
+            i++;
+        }
+        //exclude
+        solve(candidates,target,ans,op,i+1,n);
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
