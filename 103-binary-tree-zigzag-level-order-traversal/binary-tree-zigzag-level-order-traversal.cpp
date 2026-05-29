be5edcde6174aa133a8de1f973f1af0d20148191
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-05-29 11:33:16 +0530

    Time: 2 ms (12.57%) | Memory: 15 MB (77.14%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,40 +12,41 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-      vector<vector<int>> result;
+      vector<vector<int>> ans;
       if(root==NULL){
-        return result;
-      }  
-      queue<TreeNode*> q;
-      q.push(root);
-      //flag for direction
-      bool leftToright=true;
-      while(!q.empty()){
-        //har level ko process karna hai
-        //har level ka size
-        int size=q.size();
-        vector<int> ans(size);
-        for(int i=0;i<size;i++){
-            TreeNode* frontnode=q.front();
-            q.pop();
-            //front ki node ko pop karao
-            //agar true hai toh seedhe or false hai toh reverse 
-
-            int index=leftToright ? i : (size-i-1);
-            ans[index]=frontnode->val;
-            if(frontnode->left)
-                q.push(frontnode->left);
-            if(frontnode->right)
-                q.push(frontnode->right);
-        }
-        //change direction
-        leftToright=!leftToright;
-        //push elements into result
-        
-        result.push_back(ans);
-
-        
+        return ans;
       }
-      return result;
+      bool flag=true;
+      queue<TreeNode* > q;
+      q.push(root);
+      
+      
+    while(!q.empty()){
+        int n=q.size();
+        vector<int> res(n);
+            for(int i=0;i<n;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            
+                if(flag){
+                    res[i]=node->val;
+                }
+                else{
+                    res[n-i-1]=node->val;
+                }
+                if (node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            
+        
+        ans.push_back(res);
+        flag=!flag;
+
+    }
+    return ans;
     }
 };
