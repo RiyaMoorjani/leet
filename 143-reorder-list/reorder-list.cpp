5aac29f02fd90762a3fda1df9b774283ea17bd31
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-06-05 10:33:13 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (95.85%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* forward=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+
+    }
+    ListNode* middle(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    void merge(ListNode* l1,ListNode* l2){
+        while(l1!=NULL && l2!=NULL){
+            ListNode* next1=l1->next;
+            ListNode* next2=l2->next;
+            l1->next=l2;
+            if(next1 ==NULL) break;
+            l2->next=next1;
+            l1=next1;
+            l2=next2;
+
+
+        }
+    }
+    void reorderList(ListNode* head) {
+        ListNode* mid=middle(head);
+        //break into 2 list
+        ListNode* left=head;
+        ListNode* right=mid->next;
+        mid->next=NULL;
+        right=reverse(right);
+        merge(left,right);
+        
+    }
+};
