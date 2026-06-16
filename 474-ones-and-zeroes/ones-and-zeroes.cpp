80590927eeb62e008670652a414b1bd778366160
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-06-16 09:11:05 +0530

    Time: 143 ms (51.22%) | Memory: 106.1 MB (31.34%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dp[601][101][101];
-    int solve(vector<string>& strs,int i, int m, int n){
+    
+    int solve(vector<string>& strs,int i, int m, int n,vector<vector<vector<int>>> & dp){
         if(i==strs.size()){
             return 0;
         }
@@ -17,16 +17,17 @@ public:
                 ones++;
             }
         }
-        int not_take=solve(strs,i+1,m,n);
+        int not_take=solve(strs,i+1,m,n,dp);
         int take=0;
         if(m>=zeros && n>=ones){
-            take=1+solve(strs,i+1,m-zeros,n-ones);
+            take=1+solve(strs,i+1,m-zeros,n-ones,dp);
         }
         dp[i][m][n]=max(not_take,take);
         return dp[i][m][n];
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(strs,0,m,n);
+        int size=strs.size();
+        vector<vector<vector<int>>> dp(size,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        return solve(strs,0,m,n,dp);
     }
 };
