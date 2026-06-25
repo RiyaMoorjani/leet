b7827fa135ba63638df22b473d675bf154b2a592
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-06-25 22:45:07 +0530

    Time: 390 ms (11.11%) | Memory: 421.2 MB (8.93%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -6,13 +6,15 @@ public:
         for(int i=0;i<nums.size();i++){
             int cnt=0;
             int size=0;
+            vector<int> ans;
             for(int j=i;j<nums.size();j++){
                 
                 if(nums[j]==target){
                     cnt++;
                 }
-                size++;
-                if(cnt>size/2){
+                ans.push_back(nums[j]);
+                int s=ans.size();
+                if(cnt>s/2){
                 res++;
                 }
 
